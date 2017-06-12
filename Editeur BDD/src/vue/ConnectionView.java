912ef450a96f26e60ed1fd68ddb265a7e9c1711b
Gauthier Bastard <gauthb56@aol.fr>
2017-06-12 16:35:33 +0200

    creationCompte

--- Editeur BDD/src/vue/ConnectionView.java
+++ Editeur BDD/src/vue/ConnectionView.java
@@ -9,41 +9,49 @@ public class ConnectionView extends JPanel{
 	private JLabel lPseudo;
 	private JLabel lMotDePasse;
 	private JTextField fPseudo;
-	private JTextField fMotDePasse;
+	private JPasswordField fMotDePasse;
 	private JButton bConnection;
 	private JButton bCreationUtilisateur;
 	private JButton lMotDePasseOublie;
 	private JPanel panneau;
+	private JPanel panneauBouton;
 	
 	public ConnectionView(){
 		decoration();
-		panneau.add(bConnection);
-		panneau.add(bCreationUtilisateur);
-		panneau.add(lMotDePasseOublie);
-		this.add(lConnection);
-		this.add(lPseudo);
-		this.add(fPseudo);
-		this.add(lMotDePasse);
-		this.add(fMotDePasse);
+		panneauBouton.add(bConnection);
+		panneauBouton.add(bCreationUtilisateur);
+		panneauBouton.add(lMotDePasseOublie);
+		panneau.add(lConnection);
+		panneau.add(lPseudo);
+		panneau.add(fPseudo);
+		panneau.add(lMotDePasse);
+		panneau.add(fMotDePasse);
+		panneau.add(panneauBouton);
+		this.add(new JLabel("       "),BorderLayout.WEST);
 		this.add(panneau);
+		this.add(new JLabel("       "),BorderLayout.EAST);
 	}
 	
 	public void decoration(){
-		lConnection = new JLabel("<html><center>CONNEXION</center></html>");
+		this.setLayout(new BorderLayout());
+		panneau = new JPanel();
+		panneau.setLayout(new GridLayout(6,1,5,10));
+		lConnection = new JLabel("CONNEXION</center></html>");
 		lConnection.setHorizontalAlignment(SwingConstants.CENTER);
 		lPseudo = new JLabel("Utilisateur");
+		lPseudo.setHorizontalAlignment(SwingConstants.CENTER);
 		lMotDePasse = new JLabel("Mot de passe");
+		lMotDePasse.setHorizontalAlignment(SwingConstants.CENTER);
 		fPseudo = new JTextField();
-		fMotDePasse = new JTextField();
+		fMotDePasse = new JPasswordField();
 		bConnection = new JButton("Connexion");
 		bCreationUtilisateur = new JButton("Creation de compte");
 		lMotDePasseOublie = new JButton("<html><u>Mot de passe oublie</u></html>");
-		panneau = new JPanel();
-		panneau.setLayout(new GridLayout(2,2));
+		panneauBouton = new JPanel();
+		panneauBouton.setLayout(new GridLayout(2,2,5,5));
 		lMotDePasseOublie.setBorderPainted(false);
 		lMotDePasseOublie.setContentAreaFilled(false);
 		lMotDePasseOublie.setForeground(new Color(142,162,198));
-		this.setLayout(new GridLayout(6,1));
 	}
 	
 	public static void main(String[] args){
--- Editeur BDD/src/vue/CreationUserView.java
+++ Editeur BDD/src/vue/CreationUserView.java
@@ -1,5 +1,90 @@
 package vue;
 
-public class CreationUserView {
+import javax.swing.*;
+import java.awt.*;
 
+public class CreationUserView extends JPanel{
+	
+	private JLabel lTitre;
+	private JLabel lUtilisateur;
+	private JLabel lEmail;
+	private JLabel lMotDePasse;
+	private JLabel lConfirmation;
+	
+	private JLabel iUtilisateur;
+	private JLabel iEmail;
+	private JLabel iMotDePasse;
+	private JLabel iConfirmation;
+	private JTextField fUtilisateur;
+	private JTextField fEmail;
+	private JPasswordField fMotDePasse;
+	private JPasswordField fConfirmation;
+	private JButton bCreation;
+	private JPanel panneau;
+	private JPanel panneauChamps;
+	private JPanel verif;
+	
+	public CreationUserView(){
+		decoration();
+		panneauChamps.add(lTitre);
+		panneauChamps.add(lUtilisateur);
+		panneauChamps.add(fUtilisateur);
+		panneauChamps.add(lEmail);
+		panneauChamps.add(fEmail);
+		panneauChamps.add(lMotDePasse);
+		panneauChamps.add(fMotDePasse);
+		panneauChamps.add(lConfirmation);
+		panneauChamps.add(fConfirmation);
+		panneauChamps.add(bCreation);
+		
+		verif.add(new JLabel(""));
+		verif.add(new JLabel(""));
+		verif.add(iUtilisateur);
+		verif.add(new JLabel(""));
+		verif.add(iEmail);
+		verif.add(new JLabel(""));
+		verif.add(iMotDePasse);
+		verif.add(new JLabel(""));
+		verif.add(iConfirmation);
+		verif.add(new JLabel(""));
+		panneau.add(panneauChamps,BorderLayout.CENTER);
+		panneau.add(verif,BorderLayout.EAST);
+		this.add(new JLabel("       "),BorderLayout.WEST);
+		this.add(panneau);
+		this.add(new JLabel("       "),BorderLayout.EAST);
+	}
+	
+	public void decoration(){
+		this.setLayout(new BorderLayout());
+		panneauChamps = new JPanel();
+		panneauChamps.setLayout(new GridLayout(10,1,5,5));
+		panneau = new JPanel();
+		panneau.setLayout(new BorderLayout());
+		verif = new JPanel();
+		verif.setLayout(new GridLayout(10,1,5,10));
+		lTitre = new JLabel("CREATION DE COMPTE");
+		lTitre.setHorizontalAlignment(SwingConstants.CENTER);
+		lUtilisateur = new JLabel("Utilisateur");
+		lEmail = new JLabel("E-Mail");
+		lMotDePasse = new JLabel("Mot de passe");
+		lConfirmation = new JLabel("Confirmation de mot de passe");
+		iUtilisateur = new JLabel();
+		iEmail = new JLabel();
+		iMotDePasse = new JLabel();
+		iConfirmation = new JLabel();
+		fUtilisateur = new JTextField();
+		fEmail = new JTextField();
+		fMotDePasse = new JPasswordField();
+		fConfirmation = new JPasswordField();
+		bCreation = new JButton("Créer");
+		
+	}
+	
+	public static void main(String[] args){
+		JFrame fenetre = new JFrame("Connexion");
+		fenetre.setContentPane(new CreationUserView());
+		fenetre.setVisible(true);
+		fenetre.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		fenetre.pack();
+	}
 }
