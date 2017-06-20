704fec11b868362d6bb637a4e5252f3603844496
Gauthier Bastard <gauthb56@aol.fr>
2017-06-20 16:17:14 +0200

    chercher en cours

--- Editeur BDD/src/controleur/TouchePresseListener.java
+++ Editeur BDD/src/controleur/TouchePresseListener.java
@@ -2,7 +2,10 @@ package controleur;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Vector;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import vue.Fenetre;
@@ -23,18 +26,40 @@ public class TouchePresseListener implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getSource() instanceof JTextField){
-			String nom = this.fenetre.getVuePrincipale().getfChercher().getName();
-			if(nom.equals("BarreRecherche")){
-				if(e.getKeyCode()== KeyEvent.VK_ENTER){
-					
-				}
-			}
-		}
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		ArrayList<Integer> lignesValides = new ArrayList<Integer>();
+		fenetre.getVuePrincipale().insererValeursDansTab(new String[][]{{"5","5","5"},{"2","3","4"}},new String[]{"1","2","3"});
+		if(e.getSource() instanceof JTextField){
+			String nom = this.fenetre.getVuePrincipale().getfChercher().getName();
+			if(nom.equals("BarreRecherche")){
+				if(fenetre.getVuePrincipale().getDm() == null){
+					JOptionPane messageTableau = new JOptionPane();
+					messageTableau.showMessageDialog(null, "Table inexistante", "Erreur", JOptionPane.ERROR_MESSAGE);
+				}
+				else {
+					for(int i =0; i<fenetre.getVuePrincipale().getDm().getRowCount();i++){
+						for(int j=0; j<fenetre.getVuePrincipale().getDm().getColumnCount()-2; j++){
+							if(((String)fenetre.getVuePrincipale().getTable().getValueAt(i,j)).contains(fenetre.getVuePrincipale().getfChercher().getText())){
+								lignesValides.add(i);
+							}
+						}
+					}
+					System.out.println(lignesValides.toString());
+					for(int k = 0; k<fenetre.getVuePrincipale().getDm().getRowCount(); k++){
+						System.out.println(lignesValides.contains(k));
+						System.out.println(k);
+						if(!lignesValides.contains(k)){
+							fenetre.getVuePrincipale().getDm().removeRow(k);
+						}
+					}
+					fenetre.getFenetre().setVisible(true);
+				}
+			}
+		}
 	}
 
 }
