ee0e9a3872de8bfc91ebfba4a633ba1f0460012d
Gauthier Bastard <gauthb56@aol.fr>
2017-06-21 09:09:32 +0200

    recherche

--- Editeur BDD/src/controleur/TouchePresseListener.java
+++ Editeur BDD/src/controleur/TouchePresseListener.java
@@ -31,7 +31,6 @@ public class TouchePresseListener implements KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		ArrayList<Integer> lignesValides = new ArrayList<Integer>();
 		fenetre.getVuePrincipale().insererValeursDansTab(new String[][]{{"5","5","5"},{"2","3","4"}},new String[]{"1","2","3"});
 		if(e.getSource() instanceof JTextField){
 			String nom = this.fenetre.getVuePrincipale().getfChercher().getName();
@@ -41,22 +40,7 @@ public class TouchePresseListener implements KeyListener{
 					messageTableau.showMessageDialog(null, "Table inexistante", "Erreur", JOptionPane.ERROR_MESSAGE);
 				}
 				else {
-					for(int i =0; i<fenetre.getVuePrincipale().getDm().getRowCount();i++){
-						for(int j=0; j<fenetre.getVuePrincipale().getDm().getColumnCount()-2; j++){
-							if(((String)fenetre.getVuePrincipale().getTable().getValueAt(i,j)).contains(fenetre.getVuePrincipale().getfChercher().getText())){
-								lignesValides.add(i);
-							}
-						}
-					}
-					System.out.println(lignesValides.toString());
-					for(int k = 0; k<fenetre.getVuePrincipale().getDm().getRowCount(); k++){
-						System.out.println(lignesValides.contains(k));
-						System.out.println(k);
-						if(!lignesValides.contains(k)){
-							fenetre.getVuePrincipale().getDm().removeRow(k);
-						}
-					}
-					fenetre.getFenetre().setVisible(true);
+					fenetre.getVuePrincipale().getDm().rechercher(fenetre.getVuePrincipale().getfChercher().getText());
 				}
 			}
 		}
--- Editeur BDD/src/vue/ModeleTable.java
+++ Editeur BDD/src/vue/ModeleTable.java
@@ -1,5 +1,7 @@
 package vue;
 
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.table.DefaultTableModel;
 public class ModeleTable extends DefaultTableModel {
@@ -25,6 +27,8 @@ public class ModeleTable extends DefaultTableModel {
 	}
 
 	public Object getValueAt(int rowIndex, int columnIndex) {
+		System.out.println(rowIndex);
+		System.out.println(columnIndex);
 		return super.getValueAt(rowIndex, columnIndex);
 	}
 	
@@ -63,5 +67,30 @@ public class ModeleTable extends DefaultTableModel {
 	
 	public void setCellNonEditable(){
 		editable = false;
+	}
+	
+	public void rechercher(String texte){
+		//System.out.println(getValueAt(0,0));
+		ArrayList<Integer> lignesValides = new ArrayList<Integer>();
+		//System.out.println(getRowCount());
+		for(int i = 0; i<getRowCount()-1;i++){
+			for(int j=0; j<getColumnCount()-2; j++){
+				//System.out.println(getValueAt(i,j));
+				if(((String)getValueAt(i,j)).contains(texte)){
+					lignesValides.add(i);
+				}
+			}
+		}
+		//System.out.println(lignesValides.toString());
+		int l = getRowCount();
+		for(int k = 0; k<l; k++){
+			//System.out.println(lignesValides.contains(k));
+			//System.out.println(k);
+			if(!lignesValides.contains(k)){
+				removeRow(k);
+				l--;
+			}
+		}
+		fenetre.getFenetre().setVisible(true);
 	}
 }
